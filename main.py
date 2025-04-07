98c296a496d595a4d9f04fd9eb09413c33943c9a
adfre18 <44666284+adfre18@users.noreply.github.com>
2025-04-07 10:53:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ def main():
     df = load_dataset_to_dataframe(path)
 
     # iterate over all tasks
-    for task in [Task1(), Task2(), Task3()]:
-        logger.info(f'Starting task: {task.__class__.__name__} ...')
-        task.run(df)
-        logger.info(f'Finished task: {task.__class__.__name__} ...')
+    #for task in [Task1(), Task2(), Task3()]:
+    #    logger.info(f'Starting task: {task.__class__.__name__} ...')
+    #    task.run(df)
+    #    logger.info(f'Finished task: {task.__class__.__name__} ...')
 
     logger.info('All tasks completed successfully!')
 
     # Start the streamlit app
     logger.info('Starting streamlit app to visualize results...')
-    subprocess.run([streamlit_exe_filepath, "run", streamlit_filepath_webapp], check=True)
+    subprocess.run([streamlit_exe_filepath, "run", streamlit_filepath_webapp, "--server.port", "8501"], check=True)
 
 
 if __name__ == '__main__':
